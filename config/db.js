9d6551f0c63e1c4f6411f84d692897d15a50dc3f
Krishang Saxena <krishangsaxena12345@gmail.com>
2025-01-23 16:18:00 +0530

    Configur the db

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        await mongoose.connect('mongodb://localhost:27017/ecommerce', {
+        await mongoose.connect('mongodb+srv://krishangsaxena12345:ruPV0XmqTM9qEq1p@cluster0.vr9xe.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', {
             useNewUrlParser: true,
             useUnifiedTopology: true
         });
